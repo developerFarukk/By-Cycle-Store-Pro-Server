4205f712dd20fb77d3e70613a27a62c992dca697
FARUK2059 <omarfaruk34552@gmail.com>
2024-11-23 16:09:55 +0600

    revenew update succefully

--- src/model/orders/order.controller.ts
+++ src/model/orders/order.controller.ts
@@ -2,11 +2,12 @@
 import { Request, Response } from "express";
 import { orderService } from "./order.service";
 import { CreateOrder } from "./order.interface";
+import Order from "./order.model";
 
 // Bycykle Order Created Functionality
 const orderBik = async (req: Request, res: Response): Promise<Response> => {
     try {
-        const { email, product, quantity, totalPrice } = req.body as CreateOrder ;
+        const { email, product, quantity, totalPrice } = req.body as CreateOrder;
 
         if (!email || !product || !quantity || !totalPrice) {
             return res.status(400).json({
@@ -34,6 +35,47 @@ const orderBik = async (req: Request, res: Response): Promise<Response> => {
     }
 };
 
+// Order Revenew Fantionality
+const calculateRevenue = async (req: Request, res: Response) => {
+    try {
+        
+        const revenueResult = await Order.aggregate([
+            {
+                $sort: { createdAt: -1 }, 
+            },
+            {
+                $limit: 1, 
+            },
+            {
+                $project: {
+                    _id: 0,
+                    totalRevenue: { $multiply: ["$totalPrice", "$quantity"] }, 
+                },
+            },
+        ]);
+
+      
+        const totalRevenue = revenueResult[0]?.totalRevenue || 0;
+
+        // Send success response
+        return res.status(200).json({
+            message: "Revenue calculated successfully",
+            status: true,
+            data: {
+                totalRevenue,
+            },
+        });
+    } catch (error) {
+        console.error("Error calculating revenue:", error);
+        return res.status(500).json({
+            message: "Failed to calculate revenue",
+            status: false,
+            error: error.message,
+        });
+    }
+}
+
 export const orderController = {
     orderBik,
+    calculateRevenue
 };
--- src/model/orders/order.router.ts
+++ src/model/orders/order.router.ts
@@ -5,5 +5,6 @@ import { orderController } from "./order.controller";
 const orderRouter = Router()
 
 orderRouter.post('/', orderController.orderBik)
+orderRouter.get("/revenue", orderController.calculateRevenue)
 
 export default orderRouter
