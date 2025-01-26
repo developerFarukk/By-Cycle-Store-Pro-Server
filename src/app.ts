3311e634d2c8d64166852554409a244023931f5e
FARUK2059 <omarfaruk34552@gmail.com>
2025-01-27 05:02:36 +0600

    Update app

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,42 @@
 
-import express, { Request, Response } from 'express'
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express'
 import bikRouter from './model/products/bik.router'
 import orderRouter from './model/orders/order.router'
+import cookieParser from 'cookie-parser';
 
 
-const app = express()
+const app: Application = express();
 
 // middleware
 app.use(express.json())
+app.use(cookieParser());
+app.use(cors({ origin: ['http://localhost:5001'] }));
+// app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
+
+
+// app.use('/api/v1', router);
 
 app.use('/api/products', bikRouter)
 
 app.use('/api/orders', orderRouter)
 
-// POST: /api/user/create-user
 
-app.get('/', (req: Request, res: Response) => {
+// Conect Server
+const getAController = (req: Request, res: Response) => {
     res.send({
         status: true,
-        message: 'By cycle Server Live ⚡',
+        message: 'Bicycle Store Server is Runing Live ⚡',
     })
-})
+}
+
+app.get('/', getAController);
+
+
+// Global Error Handelar
+// app.use(globalErrorHandler);
+
+//Not Found Page function
+// app.use(notFound);
 
 export default app
