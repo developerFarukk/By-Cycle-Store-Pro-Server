1a760b9449948e11e447e17a4800ab2484c2bb4d
FARUK2059 <omarfaruk34552@gmail.com>
2025-01-27 10:04:34 +0600

    Add Query bui;der

--- src/model/bicycles/bicycle.route.ts
+++ src/model/bicycles/bicycle.route.ts
@@ -16,5 +16,12 @@ router.post(
 );
 
 
+// All Bicycle data get route
+router.get('/',
+    // auth(USER_ROLE.superAdmin, USER_ROLE.admin, USER_ROLE.faculty, USER_ROLE.student),
+    BicycleControllers.getAllCourses
+);
+
+
 
 export const BicycleRoutes = router;
--- src/model/bicycles/bicycle.service.ts
+++ src/model/bicycles/bicycle.service.ts
@@ -1,4 +1,5 @@
 
+import QueryBuilder from "../../builder/QueryBuilder";
 import { TBicycle } from "./bicycle.interface";
 import { Bicycle } from "./bicycles.model";
 
@@ -9,8 +10,32 @@ const createBicycleIntoDB = async (payload: TBicycle) => {
 };
 
 
+// All Bicycle data Get
+const getAllBicycleFromDB = async (query: Record<string, unknown>) => {
+    const courseQuery = new QueryBuilder(
+        Bicycle.find(),
+        // .populate('preRequisiteCourses.course'),
+        query,
+    )
+        .search(CourseSearchableFields)
+        .filter()
+        .sort()
+        .paginate()
+        .fields();
+
+    const meta = await courseQuery.countTotal();
+    const result = await courseQuery.modelQuery;
+
+    return {
+        meta,
+        result,
+    };
+};
+
+
 
 export const BicycleServices = {
     createBicycleIntoDB,
+    getAllBicycleFromDB
 
 };
