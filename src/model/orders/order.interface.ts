44b21173496c2e3f4e8f95c6341418fd94334df8
FARUK2059 <omarfaruk34552@gmail.com>
2025-01-28 05:50:36 +0600

    add order model

--- src/model/orders/order.interface.ts
+++ src/model/orders/order.interface.ts
@@ -1,28 +1,33 @@
-import mongoose from "mongoose";
-import { z } from "zod";
+import { Types } from "mongoose";
+// import { z } from "zod";
 
-export interface CreateOrder {
-
-    email: string;
-    product: string;
+export interface TOrder {
+    _id?: Types.ObjectId;
+    user: Types.ObjectId;
+    productId: Types.ObjectId;
     quantity: number;
     totalPrice: number;
+    status: 'Pending' | 'Processing' | 'Shipped' | 'Delivered' | 'Cancelled';
+    paymentStatus: 'Unpaid' | 'Paid';
 }
 
-export const createOrderValid = z.object({
-    email: z.string().email("Invalid Email Address"),
-    product: z.string(),
-    quantity: z.number().min(1, "Quantity must be at least 1"),
-    totalPrice: z.number().min(0, "Total price must be positive")
-})
 
-export interface OrderDocument extends Document {
-    status: boolean
-    email: string;
-    product: mongoose.Types.ObjectId;
-    quantity: number;
-    totalPrice: number;
 
-}
+
+// export const createOrderValid = z.object({
+//     email: z.string().email("Invalid Email Address"),
+//     product: z.string(),
+//     quantity: z.number().min(1, "Quantity must be at least 1"),
+//     totalPrice: z.number().min(0, "Total price must be positive")
+// })
+
+// export interface OrderDocument extends Document {
+//     status: boolean
+//     email: string;
+//     product: mongoose.Types.ObjectId;
+//     quantity: number;
+//     totalPrice: number;
+
+// }
 
 
--- src/model/orders/order.model.ts
+++ src/model/orders/order.model.ts
@@ -1,44 +1,44 @@
 
-import { model, Schema, version } from 'mongoose';
-import { OrderDocument } from './order.interface';
+import { model, Schema } from 'mongoose';
+import { TOrder } from './order.interface';
 
-const orderSchema = new Schema<OrderDocument>({
+const orderSchema = new Schema<TOrder>({
 
-    email: {
-        type: String,
-        required: [true, 'Please provide your email'],
-        unique: true,
-        validate: {
-            validator: function (value: string) {
-                return /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(value)
-            },
-            message: '{VALUE} is not a valid email',
-        },
-        immutable: true,
-    },
-
-    product: {
+    user: {
         type: Schema.Types.ObjectId,
-        ref: 'Product', 
-        required: [true, 'Product ID is required'],
+        ref: "User",
+        required: [true, 'User id is required']
+    },
+    productId: {
+        type: Schema.Types.ObjectId,
+        ref: "Bicycle",
+        required: [true, 'Bicycle id is required']
     },
     quantity: {
         type: Number,
-        required: [true, 'Quantity is required'],
-        min: [1, 'Quantity must be at least 1'], 
+        required: true,
+        min: [1, "Quantity must be at least 1"],
     },
     totalPrice: {
         type: Number,
-        required: [true, 'Total price is required'],
-        min: [0, 'Total price must be a positive number'],
+        required: [true, 'Total price is required']
+    },
+    status: {
+        type: String,
+        enum: ['Pending', 'Processing', 'Shipped', 'Delivered', 'Cancelled'],
+        default: 'Pending',
+    },
+    paymentStatus: {
+        type: String,
+        enum: ['Unpaid', 'Paid'],
+        default: 'Unpaid',
     },
 },
     {
         timestamps: true,
-        versionKey: false
     }
-    
+
 )
 
-const Order = model<OrderDocument>('order', orderSchema)
+const Order = model<TOrder>('Order', orderSchema)
 export default Order;
