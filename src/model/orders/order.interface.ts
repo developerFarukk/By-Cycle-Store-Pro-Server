4a615b4c97c82f50476e6ca5260b5f74a3d50690
FARUK2059 <omarfaruk34552@gmail.com>
2025-02-13 15:17:47 +0600

    update order interface

--- src/model/orders/order.interface.ts
+++ src/model/orders/order.interface.ts
@@ -1,14 +1,14 @@
 import { Types } from "mongoose";
 
-export interface TOrder {
-    _id?: Types.ObjectId;
-    user: Types.ObjectId;
-    productId: Types.ObjectId;
-    quantity: number;
-    totalPrice: number;
-    status: 'Pending' | 'Processing' | 'Shipped' | 'Delivered' | 'Cancelled';
-    paymentStatus: 'Unpaid' | 'Paid';
-}
+// export interface TOrder {
+//     _id?: Types.ObjectId;
+//     user: Types.ObjectId;
+//     productId: Types.ObjectId;
+//     quantity: number;
+//     totalPrice: number;
+//     status: 'Pending' | 'Processing' | 'Shipped' | 'Delivered' | 'Cancelled';
+//     paymentStatus: 'Unpaid' | 'Paid';
+// }
 
 
 
@@ -31,6 +31,29 @@ export interface TOrder {
 //         date_time: string;
 //     };
 // }
+
+
+export interface TOrder extends Document {
+    _id?: Types.ObjectId;
+    user: Types.ObjectId;
+    products: {
+        product: Types.ObjectId;
+        quantity: number;
+    }[];
+    totalPrice: number;
+    status: "Pending" | "Paid" | "Shipped" | "Completed" | "Cancelled";
+    transaction: {
+        id: string;
+        transactionStatus: string;
+        bank_status: string;
+        sp_code: string;
+        sp_message: string;
+        method: string;
+        date_time: string;
+    };
+    createdAt?: Date;
+    updatedAt?: Date;
+}
 
 
 
