ba7c008373da6f54bc4719ea6a8dcdc0c1d3fe4b
FARUK2059 <omarfaruk34552@gmail.com>
2025-02-16 00:34:45 +0600

    up code

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,8 @@ const app: Application = express();
 app.use(express.json())
 app.use(cookieParser());
 // app.use(cors({ origin: ['http://localhost:5001'] }));
-app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
+// app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
+app.use(cors({ origin: ['https://ridenest.vercel.app'], credentials: true }));
 
 
 // Router File Linkup
